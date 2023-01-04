ff2da722f4ff743ff2d1729f681581c977a49376
skeyanakhiyanikhil <skeyanakhiyanikhil@gamil.com>
2023-01-04 17:45:03 +0530

    add javascript

---
+++ OneDrive/Desktop/Create by NIKHIL/html files/css/proggram/Untitled-1.js
@@ -0,0 +1,18 @@
+// var age=40;
+// // if(age>90){
+// //     console.log();("true");
+// // }
+// // else{
+// //     console.log("flase")
+// // }
+
+// if(age>39){
+//     consol.log("app free ho")
+// }
+// else if(age>29){
+//     consol.log("app freee nahi ho")
+// }
+// else{
+//     consol.log("free hone ka dava kar rahe ho")
+// }
+// consol.log("happy")
