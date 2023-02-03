c6535e205485ccf982592fb0d1a2c723b614308c
skeynikhilanakhiya <skey.anakhiyanikhil@gmail.com>
2023-02-03 20:37:42 +0530

    vue .js start and new file

---
+++ OneDrive/Desktop/vue js/blog/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+<img alt="Vue logo" src="./assets/logo.png">
+<HelloWorld msg="Welcome to nikhil vue .js" />
+<home />
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import home from './components/home.vue'
+export default {
+    name: 'App',
+    components: {
+        HelloWorld,
+        home,
+    }
+}
+</script>
+
+<!-- 
+<style>
+#app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+}
+
+</style>
+-->
+<style>
+#app {
+   text-align: center;
+ }
+</style>
---
+++ OneDrive/Desktop/vue js/blog/src/components/home.vue
@@ -0,0 +1,34 @@
+<template>
+<h1>Email : {{email }}</h1>
+<h1>Mobile :{{mobile }}</h1>
+<h1>Mobile: {{mobile==8888 }}</h1>
+<h1>Name :{{"Nikhil" }}</h1>
+<h1>Name :{{getName("nikhil a") }}</h1>
+</template>
+
+<script>
+export default {
+    name: 'app',
+    data() {
+        return {
+            email: 'nikhil@test.com',
+            mobile: 90999,
+            getName: function (a) {
+                return a;
+
+            },
+            methods: {
+                getName(name) {
+                    return name;
+                }
+            }
+        }
+    }
+}
+</script>
+
+<style scoped>
+/* h1{
+    color: rgb(127, 138, 255) ;
+} */
+</style>
