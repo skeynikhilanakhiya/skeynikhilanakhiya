348ff146e81d6a54c270853e32bb696bb422d395
skeynikhilanakhiya <skey.anakhiyanikhil@gmail.com>
2023-02-08 18:02:35 +0530

    css task and vue js

--- OneDrive/Desktop/vue js/blog/src/components/home.vue
+++ OneDrive/Desktop/vue js/blog/src/components/home.vue
@@ -1,29 +1,26 @@
 <template>
-<!-- <h1>Email : {{email }}</h1> -->
-<!-- <h1>Mobile :{{mobile }}</h1> -->
+<h2 v-if="show">If Else condition</h2>
+<h2 v-else> else condition </h2>
 
-<h1 v-on:mouseenter="mouse()">home</h1>
-<button v-on:click="getData('Button click 1')">click me</button>
-<button v-on:dblclick="getData('Button  double  click ')">click me</button>
+<br>
 </template>
+
+<!-- POST METOD API -->
 
 <script>
 export default {
     name: 'app',
+    data() {
+        return {
+            show: false
 
-    methods: {
-        getData(Data) {
-            alert(Data);
         }
-    },
-    mouse() {
-       console.warn("hello");
     }
 }
 </script>
 
 <style scoped>
-/* h1{
-    color: rgb(127, 138, 255) ;
-} */
+h2 {
+    color: rgb(127, 138, 255);
+}
 </style>
