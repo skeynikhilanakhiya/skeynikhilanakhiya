99b825ef7ccd3a4e67a8ad3f056043c57bc91381
skeynikhilanakhiya <skey.anakhiyanikhil@gmail.com>
2023-02-22 16:22:47 +0530

    complate vue

---
+++ OneDrive/Desktop/vue js/blog/src/components/PageNotFound.vue
@@ -0,0 +1,18 @@
+<template>
+    <h1>4<span style="color: blue;">0</span><span style="color:violet">4</span></h1>
+</template>
+<script>
+export default {
+    name: 'PageNotFound',
+
+}
+
+</script>
+<style>
+h1 {
+    color: orange;
+}
+::selection{
+    color :blue;
+}
+</style>
